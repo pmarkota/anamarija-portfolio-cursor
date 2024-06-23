dbfd7579682eac0937fb5ce653d5bbcb34b766e4
petarmarkota <petar.markota@gmail.com>
2024-06-23 19:32:36 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { Home } from "./pages/Home";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" component={Home} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
